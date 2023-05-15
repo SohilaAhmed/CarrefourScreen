bd5c3c3f632a16c18e18c59e731d8785ad084086
Sohila <sohilaahmed668@gmail.com>
2023-05-15 12:03:13 +0300

    edit header

--- task1-design/ViewController.swift
+++ task1-design/ViewController.swift
@@ -8,15 +8,12 @@
 import UIKit
 
 
-private let reuseIdentifier1 = "item1"
-private let reuseIdentifier2 = "item2"
+
 private let reuseIdentifier3 = "item3"
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var collectionView1: UICollectionView!
     
-    @IBOutlet weak var collectionView2: UICollectionView!
     
     @IBOutlet weak var collectionView3: UICollectionView!
     
@@ -39,9 +36,7 @@ class ViewController: UIViewController {
     }
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        
-    }
+    
     
     
     @IBAction func searchAction(_ sender: UITextField) {
@@ -51,63 +46,27 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        if(collectionView == collectionView2){
-            return 1
-        }
-        if(collectionView == collectionView3){
-            return 1
-        }
         return 1
     }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if(collectionView == collectionView2){
-            return 7
-        }
-        if(collectionView == collectionView3){
-            return 7
-        }
         return 7
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        //collectionView2
-        if(collectionView == collectionView2){
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier2, for: indexPath) as! CollectionViewCell2
-            cell.label.text = "Fish"
-            
-            
-            
-            
-            return cell
-        }
+        
         
         //collectionView3
-        if(collectionView == collectionView3){
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier3, for: indexPath) as! CollectionViewCell3
-            cell.label.text = "20.00 BHD"
-            
-            
-            
-            return cell
-        }
         
-        //        collectionView1
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier1, for: indexPath) as! CollectionViewCell1
-        
-        
-        cell.img.image = UIImage(named: "Food")
-        cell.label.text = "Food"
-        
-        cell.contentView.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        cell.contentView.layer.borderColor = #colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)
-        cell.contentView.layer.borderWidth = 0.5
-        cell.contentView.layer.cornerRadius = 20
-        cell.contentView.layer.masksToBounds = true
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier3, for: indexPath) as! CollectionViewCell3
+        cell.label.text = "20.00 BHD"
         
         return cell
+        
+        
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -152,8 +111,8 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
             else { return headerView }
             
             // 4
-             
-            typedHeaderView.titleHeader.text = "items"
+            
+            
             return typedHeaderView
         default:
             // 5
@@ -161,18 +120,15 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
         }
     }
     
-
+    
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if(collectionView == collectionView2){
-            return CGSize(width: 90, height: 40)
-        }
-        if(collectionView == collectionView3){
-            return CGSize(width: 175, height: 240)
-        }
-        return CGSize(width: 150, height: 133)
+        
+        
+        return CGSize(width: 175, height: 240)
+        
         
     }
     
@@ -189,21 +145,4 @@ extension ViewController: UICollectionViewDelegateFlowLayout{
 }
 
 
-extension ViewController: UIScrollViewDelegate{
-    //    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    //        //        if scrollView == collectionView3{
-    //        //            collectionView1.isHidden = true
-    //        ////            collectionView3Constrain.constant = 0
-    //        //        }
-    //        if(scrollView.panGestureRecognizer.translation(in: scrollView.superview).y > 0)
-    //        {
-    //            print("scrolled up")
-    //            collectionView1.isHidden = false
-    //        }
-    //        else
-    //        {
-    //            print("scrolled down")
-    //            collectionView1.isHidden = true
-    //        }
-    //    }
-}
+
